e293e1f5ae54f737a5bf7f645df325e67c49c056
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2025-07-17 16:44:03 +0200

    Populate existing data structures when creating the AI

--- lua/AI/JoeBrain.lua
+++ lua/AI/JoeBrain.lua
@@ -1,4 +1,24 @@
+local NavUtils = import("/lua/sim/navutils.lua")
+
 local StandardBrain = import("/lua/aibrain.lua").AIBrain
+local StandardBrainOnCreateAI = StandardBrain.OnCreateAI
 
 ---@class JoeBrain: AIBrain
-JoeBrain = Class(StandardBrain) {}
+---@field GridReclaim AIGridReclaim
+---@field GridRecon AIGridRecon
+---@field GridPresence AIGridPresence
+JoeBrain = Class(StandardBrain) {
+
+
+    ---@param self JoeBrain
+    OnCreateAI = function(self)
+        StandardBrainOnCreateAI(self, 'NoPlan')
+
+        NavUtils.Generate()
+
+       -- requires these data structures to understand the game
+        self.GridReclaim = import("/lua/ai/gridreclaim.lua").Setup(self)
+        self.GridRecon = import("/lua/ai/gridrecon.lua").Setup(self)
+        self.GridPresence = import("/lua/ai/gridpresence.lua").Setup(self)
+    end,
+}
