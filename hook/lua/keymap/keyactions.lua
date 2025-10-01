7246ceba324bcb0333a7c4b015099d1d00659e0f
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2025-10-01 16:40:52 +0200

    Rename 'KeyActions' to just 'Actions'

--- hook/lua/keymap/keyactions.lua
+++ hook/lua/keymap/keyactions.lua
@@ -5,37 +5,37 @@ do
     local modKeyActions = {}
 
     modKeyActions['average_joe_ai_create_chunk_template_4'] = {
-        action = 'UI_Lua import("/mods/fa-joe-ai/lua/ui/KeyActions/CreateBaseChunkTemplate.lua").Handle(4)',
+        action = 'UI_Lua import("/mods/fa-joe-ai/lua/ui/Actions/CreateBaseChunkTemplate.lua").Handle(4)',
         category = keyCategory,
     }
 
     modKeyActions['average_joe_ai_create_chunk_template_8'] = {
-        action = 'UI_Lua import("/mods/fa-joe-ai/lua/ui/KeyActions/CreateBaseChunkTemplate.lua").Handle(8)',
+        action = 'UI_Lua import("/mods/fa-joe-ai/lua/ui/Actions/CreateBaseChunkTemplate.lua").Handle(8)',
         category = keyCategory,
     }
 
     modKeyActions['average_joe_ai_create_chunk_template_16'] = {
-        action = 'UI_Lua import("/mods/fa-joe-ai/lua/ui/KeyActions/CreateBaseChunkTemplate.lua").Handle(16)',
+        action = 'UI_Lua import("/mods/fa-joe-ai/lua/ui/Actions/CreateBaseChunkTemplate.lua").Handle(16)',
         category = keyCategory,
     }
 
     modKeyActions['average_joe_ai_create_chunk_template_32'] = {
-        action = 'UI_Lua import("/mods/fa-joe-ai/lua/ui/KeyActions/CreateBaseChunkTemplate.lua").Handle(32)',
+        action = 'UI_Lua import("/mods/fa-joe-ai/lua/ui/Actions/CreateBaseChunkTemplate.lua").Handle(32)',
         category = keyCategory,
     }
 
     modKeyActions['average_joe_ai_create_chunk_template_64'] = {
-        action = 'UI_Lua import("/mods/fa-joe-ai/lua/ui/KeyActions/CreateBaseChunkTemplate.lua").Handle(64)',
+        action = 'UI_Lua import("/mods/fa-joe-ai/lua/ui/Actions/CreateBaseChunkTemplate.lua").Handle(64)',
         category = keyCategory,
     }
 
     modKeyActions['average_joe_ai_create_chunk_template_128'] = {
-        action = 'UI_Lua import("/mods/fa-joe-ai/lua/ui/KeyActions/CreateBaseChunkTemplate.lua").Handle(128)',
+        action = 'UI_Lua import("/mods/fa-joe-ai/lua/ui/Actions/CreateBaseChunkTemplate.lua").Handle(128)',
         category = keyCategory,
     }
 
     modKeyActions['average_joe_ai_toggle_base_chunk_dialog'] = {
-        action = 'UI_Lua import("/mods/fa-joe-ai/lua/ui/KeyActions/ToggleBaseChunkDialog.lua").Handle()',
+        action = 'UI_Lua import("/mods/fa-joe-ai/lua/ui/Actions/ToggleBaseChunkDialog.lua").Handle()',
         category = keyCategory,
     }
 
