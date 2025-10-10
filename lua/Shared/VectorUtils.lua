815e604ed32f907fe0857ce0d11886d244f38e2c
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2025-10-10 06:43:49 +0200

    Add utility function to quickly create a vector from two coordinates

--- lua/Shared/VectorUtils.lua
+++ lua/Shared/VectorUtils.lua
@@ -1,7 +1,14 @@
+--- Constructs a vector from an x and z coordinate, populating the y coordinate with the surface height.
+---@param x number
+---@param z number
+---@return Vector
+FromXZ = function(x, z)
+    return { x, GetSurfaceHeight(x, z), z }
+end
 
---- Computes a location that is `distance` away from target in the direction of the origin. 
+--- Computes a location that is `distance` away from target in the direction of the origin.
 ---
---- A practical use is the location you'll want the engineer to move to in order to build a structure at maximum build distance. In this example the origin would be the location of the engineer. The target would be the structure that you want to build. The distance is the build distance of the engineer + the footprint size of the structure. 
+--- A practical use is the location you'll want the engineer to move to in order to build a structure at maximum build distance. In this example the origin would be the location of the engineer. The target would be the structure that you want to build. The distance is the build distance of the engineer + the footprint size of the structure.
 ---@param ox number     # x coordinate of the origin in world coordinates
 ---@param oz number     # z coordinate of the origin in world coordinates
 ---@param tx number     # x coordinate of the target in world coordinates
@@ -9,7 +16,7 @@
 ---@param distance number   # how close we want to get to the second point
 ---@return number
 ---@return number
-PointCloseToXZ = function (ox, oz, tx, tz, distance)
+PointCloseToXZ = function(ox, oz, tx, tz, distance)
     -- compute direction and distance to the target.
     local dx = tx - ox
     local dz = tz - oz
