6b7c2b1e7d54e90f3a9c02f4595b245a7948f5ec
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2025-10-15 12:59:38 +0200

    Extend the behavior builder

--- lua/Sim/Behaviors/PlatoonBuilder.lua
+++ lua/Sim/Behaviors/PlatoonBuilder.lua
@@ -1,5 +1,3 @@
-local PlatoonUtils = import("/mods/fa-joe-ai/lua/Sim/Behaviors/PlatoonUtils.lua")
-
 ---@class AIPlatoonBuilder
 ---@field Brain JoeBrain
 ---@field Platoon AIPlatoonBehavior
@@ -12,25 +10,53 @@ PlatoonBuilder = Class() {
         self.Platoon = platoon
     end,
 
-    ---@param self AIPlatoonBuilder
-    ---@param input AIPlatoonBehaviorInput
-    AssignInput = function(self, input)
-        PlatoonUtils.AssignInput(self.Platoon, input)
-    end,
-
-    --- Starts the behavior of a platoon.
+    --- Starts the behavior of a platoon using the specific input as parameters.
     ---@param self AIPlatoonBuilder
     ---@param input? AIPlatoonBehaviorInput
     StartBehavior = function(self, input)
-        PlatoonUtils.StartPlatoon(self.Platoon, input)
+        local platoon = self.Platoon
+        platoon.PlatoonBehaviorInput = input or {}
+        platoon:ChangeState(platoon.Start)
         return self
     end,
 
+    --- General assignment of units. This is a convenience function that assigns units to specific squads.
     ---@param self AIPlatoonBuilder
     ---@param units JoeUnit[]
     ---@return AIPlatoonBuilder
     AssignUnits = function(self, units)
-        PlatoonUtils.AssignUnits(self.Brain, self.Platoon, units)
+        -- TODO: do proper filtering? Should this function even exist?
+        self:AssignSupportUnits(units)
+        self:AssignArtilleryUnits({})
+        self:AssignGuardUnits({})
+        self:AssignScoutUnits({})
+        self:AssignAttackUnits({})
+
+        return self
+    end,
+
+    --- Assigns units to the specified squad. Updates the platoon reference of units.
+    ---@param self AIPlatoonBuilder
+    ---@param squad PlatoonSquads
+    ---@param units JoeUnit[]
+    ---@return AIPlatoonBuilder
+    AssignUnitsToSquad = function(self, units, squad)
+        local brain = self.Brain
+        local platoon = self.Platoon
+
+        -- assertions
+        if table.empty(units) then
+            return self
+        end
+
+        brain:AssignUnitsToPlatoon(platoon, units, squad, 'None')
+
+        -- inform the unit of the event
+        for k = 1, table.getn(units) do
+            local unit = units[k]
+            unit:OnAssignedToPlatoon(platoon)
+        end
+
         return self
     end,
 
@@ -38,8 +64,7 @@ PlatoonBuilder = Class() {
     ---@param self AIPlatoonBuilder
     ---@param units JoeUnit[]
     AssignAttackUnits = function(self, units)
-        PlatoonUtils.AssignAttackUnits(self.Brain, self.Platoon, units)
-        return self
+        return self:AssignUnitsToSquad(units, "Attack")
     end,
 
     --- Assigns units to the support squad.
@@ -47,8 +72,7 @@ PlatoonBuilder = Class() {
     ---@param units JoeUnit[]
     ---@return AIPlatoonBuilder
     AssignSupportUnits = function(self, units)
-        PlatoonUtils.AssignSupportUnits(self.Brain, self.Platoon, units)
-        return self
+        return self:AssignUnitsToSquad(units, "Support")
     end,
 
     --- Assigns units to the artillery squad.
@@ -56,8 +80,7 @@ PlatoonBuilder = Class() {
     ---@param units JoeUnit[]
     ---@return AIPlatoonBuilder
     AssignArtilleryUnits = function(self, units)
-        PlatoonUtils.AssignArtilleryUnits(self.Brain, self.Platoon, units)
-        return self
+        return self:AssignUnitsToSquad(units, "Artillery")
     end,
 
     --- Assigns units to the guard squad.
@@ -65,8 +88,7 @@ PlatoonBuilder = Class() {
     ---@param units JoeUnit[]
     ---@return AIPlatoonBuilder
     AssignGuardUnits = function(self, units)
-        PlatoonUtils.AssignGuardUnits(self.Brain, self.Platoon, units)
-        return self
+        return self:AssignUnitsToSquad(units, "Guard")
     end,
 
     --- Assigns units to the scout squad.
@@ -74,14 +96,12 @@ PlatoonBuilder = Class() {
     ---@param units JoeUnit[]
     ---@return AIPlatoonBuilder
     AssignScoutUnits = function(self, units)
-        PlatoonUtils.AssignScoutUnits(self.Brain, self.Platoon, units)
-        return self
+        return self:AssignUnitsToSquad(units, "Scout")
     end,
 
     ---@param self AIPlatoonBuilder
     ---@return AIPlatoonBehavior
     End = function(self)
-        -- TODO: basic validation
         return self.Platoon
     end,
 }
@@ -90,7 +110,11 @@ PlatoonBuilder = Class() {
 ---@param brain JoeBrain
 ---@return AIPlatoonBuilder
 Build = function(brain, behavior)
-    local platoon = PlatoonUtils.CreatePlatoonWithBehavior(brain, behavior)
+    local platoon = brain:MakePlatoon("", "") --[[@as AIPlatoonBehavior]]
+    setmetatable(platoon, behavior)
+
+    -- initialize state of the behavior
+    platoon:OnCreate()
     return PlatoonBuilder(brain, platoon)
 end
 
